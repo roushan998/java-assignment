c70044a389eeb7196859c5c530e2be4d8ebc1ea9
roushan998 <roushankumar.h@listertechnologies.com>
2016-11-07 17:26:21 +0530

    add

---
+++ SecondProgram.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class classexample
+{
+
+int a =5;
+char ch = 'c';
+ public static void main ( String args[])
+
+{ classexample co = new classexample();
+   System.out.println(co.a);
+System.out.println(co.ch);
+}
+}
